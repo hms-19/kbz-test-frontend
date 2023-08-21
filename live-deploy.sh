76429ccbab9a0863a355d09762b4a31b3c51733c
Htet Myat Soe <74512999+hms-19@users.noreply.github.com>
2023-08-21 15:19:23 +0630

    add build

--- live-deploy.sh
+++ live-deploy.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 rm -rf ./build/;
 yarn run build;
 cd ./build/;
@@ -11,7 +10,7 @@ echo =========;
 sudo scp -i ~/.server_ssh/kbz-frontend-ec2.pem ./build.tar.gz ubuntu@ec2-54-95-229-18.ap-northeast-1.compute.amazonaws.com:.;
 echo deploying;
 echo =========;
-sudo ssh -i ~/.server_ssh/kbz-frontend-ec2.pem  ubuntu@ec2-54-95-229-18.ap-northeast-1.compute.amazonaws.com 'cd /var/www/kbz-test-frontend/; sudo rm *; sudo mv ~/build.tar.gz .; sudo tar xvf ./build.tar.gz; sudo rm build.tar.gz;';
+sudo ssh -i ~/.server_ssh/kbz-frontend-ec2.pem  ubuntu@ec2-54-95-229-18.ap-northeast-1.compute.amazonaws.com 'cd kbz-test-frontend/; sudo rm *; sudo mv ~/build.tar.gz .; sudo tar xvf ./build.tar.gz; sudo rm build.tar.gz;';
 echo deploying finished;
 echo ==================;
 cd ..;
